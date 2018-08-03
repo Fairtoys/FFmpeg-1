07d4164e301fa63f1da0aad292af0bc663786c08
wangjunchao-hj@huajiao.tv <wangjunchao-hj@huajiao.tv>
2018-08-03 18:01:25 +0800

    修改ffplay

---
+++ FFDebug_Demo/FFDebug_Demo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FFDebug_Demo
+//
+//  Created by huajiao on 2018/8/3.
+//  Copyright © 2018年 HuaJiao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
---
+++ FFPlayDebug/FFPlayDebug/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FFPlayDebug
+//
+//  Created by huajiao on 2018/3/13.
+//  Copyright © 2018年 huajiao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
